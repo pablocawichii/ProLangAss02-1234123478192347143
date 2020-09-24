2add5d900841814fa36703b5c8f12877875ccbdc
pablocawichii <pablocawichii@gmail.com>
2020-09-24 13:02:54 -0600

    Added tokenization.

--- ProgLangAss02/Program.d
+++ ProgLangAss02/Program.d
@@ -1,15 +1,61 @@
 import std;
+import std.typecons : Yes;
 
-int find(string ins) {
-    for(int i = to!int(ins.length) -1; i >= 0; i--) {
-    	writeln(ins[i]);
-    }
+
+//open rec r1,r1;tri r1,r1,r1 ; rec r1, r1  close
+
+int lastIndexOf(string[] tokens, string text, int start = -1) {
+	if(start < 0 || start >= tokens.length){
+		start = tokens.length - 1;
+	}
+    for(int i = start; i >= 0; i--){
+	    if(tokens[i].equal(text)){
+			return i;
+		}
+	}
+	return -1;
+}
+
+string[] tokenizeStr(string str) {
+    string[] a = str.split(regex("[\r\n\t\f\v, ]"));
+
+    for(int i = a.length - 1; i >= 0 ; i--) {
+		if(a[i].indexOf(';') != -1){
+
+            string[] b = a[i].split(";");
+            for(int k = 0; k < b.length; k++) {
+				if(b[k].equal("")){
+				    b[k]=";";
+					break;
+				}
+                if(k == 1) {
+				    b.insertInPlace(1, ";");
+				}
+			}
+            a = a.replace(i, i+1, b);
+		}
+	}
+
+    for(int i = a.length - 1; i >= 0; i--){
+	    if(a[i].equal("")){
+		    a = a.remove(i);
+		}
+	}
+
+    writeln(a);
+    return a;
+}
+
+int checkGrammar(string[] tokens) {
+    int nextCheck = lastIndexOf(tokens, ";", 8);
+	if(nextCheck == -1){
+		nextCheck = 1;
+	}
+
+	writeln(nextCheck);
+
     return 0;
 }
-
-void programErrors(string str) {
-}
-
 
 void main()
 {
@@ -20,7 +66,12 @@ void main()
         if(str == "EXIT\n"){
         	break;
         }
-    	programErrors(str);
+    	string[] tokens = tokenizeStr(str);
+
+        if(checkGrammar(tokens) != -1){
+		    
+		} 
+
 		writeln("Please enter the code: ");
     }
 
